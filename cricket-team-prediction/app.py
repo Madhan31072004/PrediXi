57bb95cf78f667000b7a59b0889b5c93e92e9f0d
MadhanGopaladas <113770842+Madhan31072004@users.noreply.github.com>
2025-04-10 23:09:56 +0530

    Update app.py

--- cricket-team-prediction/app.py
+++ cricket-team-prediction/app.py
@@ -274,9 +274,6 @@ def home():
 @app.route('/register')
 def register():
     return render_template("register.html")
-@app.route('/predixi')
-def register():
-    return render_template("/admin/predixi.html")
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
